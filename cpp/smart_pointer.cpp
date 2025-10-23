d2895f1a0262c681c7d9e5691d299b6af3c60147
MannixYang <104436418+MannixYang@users.noreply.github.com>
2025-10-23 17:26:56 +0800

    smart

--- cpp/smart_pointer.cpp
+++ cpp/smart_pointer.cpp
@@ -12,8 +12,13 @@ public:
     static std::unique_ptr<BigInt> CreateUnique(int n){
         return std::make_unique<BigInt>(n);
     }
+/**
+ * 创建一个BigInt引用
+ * @param n 初始化的整数值
+ * @return BigInt对象的引用
+ */
     static BigInt& CreateRef(int n){
-        return *CreateRaw(n);
+        return *CreateRaw(n);  // 通过CreateRaw创建原始指针并返回其引用
     }
     static BigInt Create(int n){
         return BigInt(n);
@@ -221,47 +226,94 @@ void* CS_BigInt_my_add_ptr(void* a, CSHandleType a_type, void* b, CSHandleType b
     return N_BigInt_my_add_ptr(a_raw, b_raw);
 }
 
-void* CS_BigInt_my_add_shared(void* a, CSHandleType a_type, void* b, CSHandleType b_type) {
-    void * a_shared =  nullptr;
-    if (a_type == CSHandleTypeRaw)
-        a_shared = new std::shared_ptr<BigInt>(reinterpret_cast<BigInt*>(a));
-    else if (a_type == CSHandleTypeShared)
-        a_shared = reinterpret_cast<std::shared_ptr<BigInt>*>(a);
-    else if (a_type == CSHandleTypeUnique)
-        a_shared = new std::shared_ptr<BigInt>(reinterpret_cast<std::unique_ptr<BigInt>*>(a)->release());
+// void* CS_BigInt_my_add_shared(void* a, CSHandleType a_type, void* b, CSHandleType b_type) {
+//     void * a_shared =  nullptr;
+//     if (a_type == CSHandleTypeRaw)
+//         a_shared = new std::shared_ptr<BigInt>(reinterpret_cast<BigInt*>(a));
+//     else if (a_type == CSHandleTypeShared)
+//         a_shared = reinterpret_cast<std::shared_ptr<BigInt>*>(a);
+//     else if (a_type == CSHandleTypeUnique)
+//         a_shared = new std::shared_ptr<BigInt>(reinterpret_cast<std::unique_ptr<BigInt>*>(a)->release());
 
-    void * b_raw =  nullptr;
-    if (b_type == CSHandleTypeRaw)
-        b_raw = b;
-    else if (b_type == CSHandleTypeShared)
-        b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(b)->get();
-    else if (b_type == CSHandleTypeUnique)
-        b_raw = reinterpret_cast<std::unique_ptr<BigInt>*>(b)->get();
+//     void * b_raw =  nullptr;
+//     if (b_type == CSHandleTypeRaw)
+//         b_raw = b;
+//     else if (b_type == CSHandleTypeShared)
+//         b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(b)->get();
+//     else if (b_type == CSHandleTypeUnique)
+//         b_raw = reinterpret_cast<std::unique_ptr<BigInt>*>(b)->get();
 
-    auto result = N_BigInt_my_add_shared(a_shared, b_raw);
-    return new std::shared_ptr<BigInt>(*reinterpret_cast<std::shared_ptr<BigInt>*>(result));
+//     auto result = N_BigInt_my_add_shared(a_shared, b_raw);
+//     return new std::shared_ptr<BigInt>(*reinterpret_cast<std::shared_ptr<BigInt>*>(result));
+// }
+
+void* CS_BigInt_my_add_shared(void* a, void* b) {
+    auto arg_a = reinterpret_cast<CS_BigInt*>(a);
+    auto arg_b = reinterpret_cast<CS_BigInt*>(b);
+
+    // 处理 a 参数
+    std::shared_ptr<BigInt> a_shared;
+    auto deleter = [](BigInt*){};
+
+    if (arg_a->type == CSHandleTypeRaw) 
+        // raw -> shared
+        a_shared = std::shared_ptr<BigInt>(reinterpret_cast<BigInt*>(arg_a->self), deleter);
+    else if (arg_a->type == CSHandleTypeShared) 
+        // shared -> shared
+        a_shared = *reinterpret_cast<std::shared_ptr<BigInt>*>(arg_a->self);
+    else if (arg_a->type == CSHandleTypeUnique) 
+        // unique -> shared
+        a_shared = std::shared_ptr<BigInt>(reinterpret_cast<std::unique_ptr<BigInt>*>(arg_a->self)->get(), deleter);
+
+    // 处理 b 参数
+    BigInt* b_raw = nullptr;
+    if (arg_b->type == CSHandleTypeRaw)
+        // raw -> raw
+        b_raw = reinterpret_cast<BigInt*>(arg_b->self);
+    else if (arg_b->type == CSHandleTypeShared)
+        // shared -> raw
+        b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(arg_b->self)->get();
+    else if (arg_b->type == CSHandleTypeUnique)
+        // unique -> raw
+        b_raw = reinterpret_cast<std::unique_ptr<BigInt>*>(arg_b->self)->get();
+
+    void* res = N_BigInt_my_add_shared(&a_shared, b_raw);
+    return new CS_BigInt{res, CSHandleTypeShared};
 }
 
-void* CS_BigInt_my_add_unique(void* a, CSHandleType a_type, void* b, CSHandleType b_type) {
-    void * a_unique =  nullptr;
-    if (a_type == CSHandleTypeRaw)
-        a_unique = new std::unique_ptr<BigInt>(reinterpret_cast<BigInt*>(a));
-    else if (a_type == CSHandleTypeShared){
-        a_unique = new std::unique_ptr<BigInt>(reinterpret_cast<std::shared_ptr<BigInt>*>(a)->get());
-    }
-    else if(a_type == CSHandleTypeUnique)
-        a_unique = new std::unique_ptr<BigInt>(std::move(*reinterpret_cast<std::unique_ptr<BigInt>*>(a)));
+// void* CS_BigInt_my_add_unique(void* a, CSHandleType a_type, void* b, CSHandleType b_type) {
+//     void * a_unique =  nullptr;
+//     if (a_type == CSHandleTypeRaw)
+//         a_unique = new std::unique_ptr<BigInt>(reinterpret_cast<BigInt*>(a));
+//     else if (a_type == CSHandleTypeShared){
+//         a_unique = new std::unique_ptr<BigInt>(reinterpret_cast<std::shared_ptr<BigInt>*>(a)->get());
+//     }
+//     else if(a_type == CSHandleTypeUnique)
+//         a_unique = new std::unique_ptr<BigInt>(std::move(*reinterpret_cast<std::unique_ptr<BigInt>*>(a)));
 
-    void * b_raw =  nullptr;
-    if (b_type == CSHandleTypeRaw)
-        b_raw = b;
-    else if (b_type == CSHandleTypeShared)
-        b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(b)->get();
-    else if (b_type == CSHandleTypeUnique)
-        b_raw = reinterpret_cast<std::unique_ptr<BigInt>*>(b)->get();
+//     void * b_raw =  nullptr;
+//     if (b_type == CSHandleTypeRaw)
+//         b_raw = b;
+//     else if (b_type == CSHandleTypeShared)
+//         b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(b)->get();
+//     else if (b_type == CSHandleTypeUnique)
+//         b_raw = reinterpret_cast<std::unique_ptr<BigInt>*>(b)->get();
 
-    return N_BigInt_my_add_unique(a_unique, b_raw);
+//     return N_BigInt_my_add_unique(a_unique, b_raw);
+// }
+// }
+
+void* CS_BigInt_my_add_unique(void* a, void* b){
+  // shared -> unique
+  std::unique_ptr<BigInt> a_unique = std::unique_ptr<BigInt>(new BigInt(*reinterpret_cast<std::shared_ptr<BigInt>*>(a)->get()));
+
+  // shared -> raw
+  BigInt* b_raw = reinterpret_cast<std::shared_ptr<BigInt>*>(b)->get();
+
+  void* result = N_BigInt_my_add_unique(&a_unique, b_raw);
+
+  // unqiue -> shared
+  return new std::shared_ptr<BigInt>(std::move(*reinterpret_cast<std::unique_ptr<BigInt>*>(a)));
+
 }
 }
-
-
